74ffe0ae9dc75eed25a373cdb8f5af1cc8a1584d
Gildemberg Leite <gildembergleite@gmail.com>
2023-12-18 21:37:46 -0300

    fix: add key_name

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "observability" {
   ami           = "ami-0759f51a90924c166"
   instance_type = "t2.micro"
+  key_name      = "new-terraform-key"
 
   tags = {
     Name = "Atividade-Observability"
